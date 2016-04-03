45ec69126d9e628607b1de5b35a060cc4db5ed97
Abhishek <amantha@usc.edu>
2016-04-03 16:22:43 -0700

    Moving code to accounts_w.php and converting links to checkboxes

--- views/dashboard.php
+++ views/dashboard.php
@@ -77,23 +77,7 @@ $email = $_SESSION["user_email"];
 				print '<a href="php/logout.php">Logout</a><br />';
 				print '<br /><h2>Accounts:</h2></br />';
 				require_once('widgets/accounts_w.php');
-			?>
-
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-xs-6">
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Account Name" aria-describeby="basic-addon2" name="accountName">
-							<span class="input-group-btn">
-								<button class="btn btn-primary" type="button" name="addAccount"> Add </button>
-								<button class="btn btn-danger" type="button" name="removeAccount"> Remove </button>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<?php
+			
 				print '<br /><h2>Transactions:</h2></br />';
 				require_once('widgets/transactions_w.php');
 			?>
--- widgets/accounts_w.php
+++ widgets/accounts_w.php
@@ -8,4 +8,18 @@ foreach ($accounts as $accountName) {
 }
 print '<br />';
 
+
+echo "<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-6">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Account Name" aria-describeby="basic-addon2" name="accountName">
+				<span class="input-group-btn">
+					<button class="btn btn-primary" type="button" name="addAccount"> Add </button>
+					<button class="btn btn-danger" type="button" name="removeAccount"> Remove </button>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>";
 ?>
