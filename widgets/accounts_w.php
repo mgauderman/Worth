9be22fbf5188c832bd64b220a4138a8db64d645f
Anush <anushkadoyan@gmail.com>
2016-04-03 16:35:10 -0700

    transactions widget

--- widgets/accounts_w.php
+++ widgets/accounts_w.php
@@ -2,24 +2,10 @@
 
 $accounts = $db->getAccounts();
 
-foreach ($accounts as $accountName) {
+foreach ($accounts as $account) {
 	print '<br />';
-	print '<a href="index.php/?tas=' . urlencode($accountName) . '">' . $accountName . '</a>';	
+	print $account;
 }
 print '<br />';
 
-
-echo "<div class="container-fluid">
-	<div class="row">
-		<div class="col-xs-6">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Account Name" aria-describeby="basic-addon2" name="accountName">
-				<span class="input-group-btn">
-					<button class="btn btn-primary" type="button" name="addAccount"> Add </button>
-					<button class="btn btn-danger" type="button" name="removeAccount"> Remove </button>
-				</span>
-			</div>
-		</div>
-	</div>
-</div>";
 ?>
--- widgets/transactions_w.php
+++ widgets/transactions_w.php
@@ -1,20 +1,61 @@
 <?php
 
-$accountName = 'Visa Credit Card';
-$transactions = $db->getTransactions($accountName);
 
-foreach($transactions as $transaction) {
-	print '<br />';
-	print $transaction['date'];
-	print '<br />';
-	print $transaction['accountName'];
-	print '<br />';
-	print $transaction['amount'];
-	print '<br />';
-	print $transaction['merchant'];
-	print '<br />';
-	print $transaction['category'];
-	print '<br />';	
+$accountsToDisplay = $db->getAccounts();
+
+
+if (isset($_GET['tas'])) {
+	$listOfAccountsToShow = urldecode($_GET['tas']);
+	$selectedAccounts = explode(",", $listOfAccountsToShow);
+
+	for ($x = 0; $x < count($selectedAccounts); $x++) {
+		$selectedAccounts[$x] = ucwords($selectedAccounts[$x]);
+	}
+
 }
-
 ?>
+ <html>
+	<body>
+		<table class="table table-striped">
+			<thead>
+			    <tr>
+			    	<th>Account</th>
+					<th>Date</th>
+					<th>Amount</th>
+					<th>Merchant</th>
+					<th>Category</th>
+			    </tr>
+
+				<tbody>
+
+					<?php
+					foreach($selectedAccounts as $account) {
+						$accountName = $account;
+						print '<tr><td>';
+						print($accountName);
+						print '</td>';
+						$transactions = $db->getTransactions($accountName);
+						foreach($transactions as $transaction) {
+							if($transaction['accountName'] == $accountName) {
+									print '<td>';
+									print $transaction['date'];
+									print '</td><td>';
+									print $transaction['amount'];
+									print '</td><td>';
+									print $transaction['merchant'];
+									print '</td><td>';
+									print $transaction['category'];
+									print '</td>';	
+							}
+
+					}
+
+					}
+
+				?>
+				</tbody>
+			</thead>
+		</table>
+	</body>
+</html>
+ 
