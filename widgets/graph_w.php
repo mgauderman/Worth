ea0940b6234059ac1ea93d4d2915aec9ee984bce
Abhishek <amantha@usc.edu>
2016-04-16 15:02:36 -0700

    ADded working graph code for sample data

--- widgets/graph_w.php
+++ widgets/graph_w.php
@@ -1,8 +1,51 @@
-<div style="font-color:red;">This is where the graph will go.</div><br />
-To put Javascript here, do this: (shown in the php file widgets/graph_w.php)
+<?php 
+?>
+<div style="font-color:red;" id="chart">
+	This is where the graph will go.
+</div>
+
+<link href="../vendors/c3.min.css" rel="stylesheet" type="text/css">
+<script src="../vendors/d3.min.js" charset="utf-8"></script>
+<script src="../vendors/c3.min.js"></script>
+
 <script type="text/javascript">
-	console.log("javascript working");
+	var cols = [
+		['data1', 30, 200, 100, 400, 150, 250],
+		['data2', 50, 20, 10, 40, 15, 25],
+		['x', 2016, 2017, 2018, 2019, 2020, 2021]
+	]; 
+
+
+	var chart = c3.generate({
+		bindto: '#chart',
+		data: {
+			x: 'x',
+			columns: cols
+			// axes: {
+			// 	data2: 'y2'
+			// }
+		},
+		axis: {
+			x: {
+				type: 'timeseries',
+				tick: {
+				 	format: '%Y-%m-%d'
+				 }
+			}
+		},
+		color: {
+			pattern: [ '#990000', '#ffff00', '#000099', '#ff6600', '#660033' ]
+		}, 
+		legend: {
+			position: 'right'
+		}
+	});
+
+	/*
+axis: {
+			
+		},
+
+	 */
 </script>
-<br />
-Check the requirements notes we have for what needs to be displayed. db_query.php will soon have all methods necessary.<br />
-They will be: getNetWorth(), addTransactions(params), getTotalAssets($startDate, $endDate), getTotalLiabilities(start, end)<br />
+
