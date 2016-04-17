e6ffc159e07819f1ef2006cd7baede553d2d7e09
Abhishek <amantha@usc.edu>
2016-04-16 18:10:28 -0700

    Graph now displays in the dashboard

--- widgets/graph_w.php
+++ widgets/graph_w.php
@@ -1,35 +1,37 @@
 <?php 
 ?>
 <div style="font-color:red;" id="chart">
-	This is where the graph will go.
 </div>
 
-<link href="../vendors/c3.min.css" rel="stylesheet" type="text/css">
-<script src="../vendors/d3.min.js" charset="utf-8"></script>
-<script src="../vendors/c3.min.js"></script>
+<link href="vendors/c3.min.css" rel="stylesheet" type="text/css">
+<script src="vendors/d3.min.js" charset="utf-8"></script>
+<script src="vendors/c3.min.js"></script>
 
 <script type="text/javascript">
 	var cols = [
 		['data1', 30, 200, 100, 400, 150, 250],
-		['data2', 50, 20, 10, 40, 15, 25],
-		['x', 2016, 2017, 2018, 2019, 2020, 2021]
+		['data2', 50, 20, 10, 40],
+		['x', 2016, 2017, 2018, 2019, 2021, 2022],
+		['x2', 2015, 2017, 2018, 2023]
 	]; 
+
+	
 
 
 	var chart = c3.generate({
 		bindto: '#chart',
 		data: {
-			x: 'x',
+			xs: {
+				'data1': 'x',
+				'data2': 'x2'
+			},
 			columns: cols
-			// axes: {
-			// 	data2: 'y2'
-			// }
 		},
 		axis: {
-			x: {
+			xs: {
 				type: 'timeseries',
 				tick: {
-				 	format: '%Y-%m-%d'
+				 	format: function(x) { return x.getFullYear(); }
 				 }
 			}
 		},
@@ -40,12 +42,6 @@
 			position: 'right'
 		}
 	});
-
-	/*
-axis: {
-			
-		},
-
-	 */
 </script>
 
+<!--var chartData = <?php echo json_encode($chartData)?>;-->
