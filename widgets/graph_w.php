2707b9f7c9a3f57415b0510781750203ea62b03a
Abhishek <amantha@usc.edu>
2016-04-17 15:46:12 -0700

    Adding jQuery calendar widgets

--- widgets/graph_w.php
+++ widgets/graph_w.php
@@ -1,13 +1,38 @@
 <?php
  
 ?>
-<div style="font-color:red;" id="chart"></div>
 
-<link href="vendors/c3.min.css" rel="stylesheet" type="text/css">
+<div style="font-color: red" id="chart-container">
+	<div id="chart"></div>
+	<p> 
+		Start Date: <input type="text" id="start-datepicker" />
+	</p>
+	<p>
+		End Date: <input type="text" id="end-datepicker" />
+	</p>
+</div>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="vendors/c3.min.css" type="text/css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="vendors/d3.min.js" charset="utf-8"></script>
 <script src="vendors/c3.min.js"></script>
 
 <script type="text/javascript">
+
+	var startDatePicker = $( "#start-datepicker" ).datepicker({
+		changeMonth: true,
+		changeYear: true
+	});
+	startDatePicker.datepicker( "option", "dateFormat", "yy-mm-dd" );
+
+	var endDatePicker = $( "#end-datepicker" ).datepicker({
+		changeMonth: true,
+		changeYear: true
+	});
+	endDatePicker.datepicker( "option", "dateFormat", "yy-mm-dd" );
+
 	var cols = [
 		['data1', 30, 200, 100, 400, 150, 250],
 		['data2', 50, 20, 10, 40],
@@ -40,5 +65,12 @@
 		}
 	});
 </script>
+
+
+
+<script type="text/javascript">
+	
+</script>
+
 
 <!--var chartData = <?php echo json_encode($chartData)?>;-->
