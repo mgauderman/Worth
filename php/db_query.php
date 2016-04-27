1038143717e7d98bae411aa862742cba183c5dd3
Utkash Dubey Linux <udubey@usc.edu>
2016-04-27 14:50:50 -0700

    database encryption added and tested

--- php/db_query.php
+++ php/db_query.php
@@ -32,7 +32,7 @@ class WorthDB {
 	public function credentialsValid($email, $password) {
 		// return true if the supplied credentials match credentials from the database, false otherwise
 
-		$query = "SELECT * FROM users WHERE email = '" . mysqli_real_escape_string($this->link, $email) . "' AND password = '" . mysqli_real_escape_string($this->link, $password) . "';";
+		$query = "SELECT * FROM users WHERE email = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $email) . "', 'thisispassword', @iv) AND password = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $password) . "', 'thisispassword', @iv);";
 		$result = $this->getQueryResult($query);
 
 		// if there is no user with the given email address and password, the credentials are invalid
@@ -51,7 +51,7 @@ class WorthDB {
 	}
 
 	public function getTransactions($accountName) {
-		$query = "SELECT accountName, merchant, amount, `date`, category FROM transactions WHERE email='" . mysqli_real_escape_string($this->link, $this->email) . "' AND accountName = '" . mysqli_real_escape_string($this->link, $accountName) . "' ORDER BY `date`;";
+		$query = "SELECT AES_DECRYPT(accountName, 'thisispassword', @iv) AS accountName, AES_DECRYPT(merchant, 'thisispassword', @iv) AS merchant, amount AS amount, `date`, AES_DECRYPT(category, 'thisispassword', @iv) AS category FROM transactions WHERE email=AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $this->email) . "', 'thisispassword', @iv) AND accountName = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $accountName) . "', 'thisispassword', @iv) ORDER BY `date`;";
 		$result = $this->getQueryResult($query);
 		$transactions = array();
 		$i = 0;
@@ -66,7 +66,7 @@ class WorthDB {
 	public function getAccounts() {
 		// returns a list of the user's accounts in alphabetical order
 		// $email should be defined by now, since credentialsValid() should have been called and returned true
-		$query = "SELECT accountName FROM accounts WHERE email = '" . mysqli_real_escape_string($this->link, $this->email) . "' ORDER BY accountName;";
+		$query = "SELECT AES_DECRYPT(accountName, 'thisispassword', @iv) AS accountName FROM accounts WHERE email = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $this->email) . "', 'thisispassword', @iv) ORDER BY AES_DECRYPT(accountName, 'thisispassword', @iv);";
 		$result = $this->getQueryResult($query);
 		$accounts = array();
 		$i = 0;
@@ -88,15 +88,15 @@ class WorthDB {
 	}
 
 	public function deleteAccount($accountName) {
-		$query = "DELETE FROM accounts WHERE email = '" . mysqli_real_escape_string($this->link, $this->email) . "' AND accountName = '" . mysqli_real_escape_string($this->link, $accountName) . "';";
+		$query = "DELETE FROM accounts WHERE email = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $this->email) . "', 'thisispassword', @iv) AND accountName = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $accountName) . "', 'thisispassword', @iv);";
 		$result = $this->getQueryResult($query);
-		$query = "DELETE FROM transactions WHERE email = '" . mysqli_real_escape_string($this->link, $this->email) . "' AND accountName = '" . mysqli_real_escape_string($this->link, $accountName) . "';";
+		$query = "DELETE FROM transactions WHERE email = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $this->email) . "', 'thisispassword', @iv) AND accountName = AES_ENCRYPT('" . mysqli_real_escape_string($this->link, $accountName) . "', 'thisispassword', @iv);";
 		$result = $this->getQueryResult($query);
 		return $result;
 	}
 
 	public function getTransactionsForGraph($startDate, $endDate, $accountName) {
-		$query = 'SELECT `date`, amount FROM transactions WHERE email = "' . mysqli_real_escape_string($this->link, $this->email) . '" AND accountName = "' . mysqli_real_escape_string($this->link, $accountName) . '" AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
+		$query = 'SELECT `date`, amount FROM transactions WHERE email = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND accountName = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $accountName) . '", "thisispassword", @iv) AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
 		$result = $this->getQueryResult($query);
 		if ($result == null) {
 			return array();
@@ -111,7 +111,7 @@ class WorthDB {
 	}
 
 	public function getTotalAssets($startDate, $endDate) {
-		$query = 'SELECT `date`, amount FROM transactions WHERE email = "' . mysqli_real_escape_string($this->link, $this->email) . '" AND asset = 1 AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
+		$query = 'SELECT `date`, amount FROM transactions WHERE email = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND asset = 1 AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
 		$result = $this->getQueryResult($query);
 		$totalAssets = array(); // map from `date` (as string) to number
 		$sum = 0;
@@ -126,7 +126,7 @@ class WorthDB {
 	}
 
 	public function getTotalLiabilities($startDate, $endDate) {
-		$query = 'SELECT `date`, amount FROM transactions WHERE email = "' . mysqli_real_escape_string($this->link, $this->email) . '" AND asset = 0 AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
+		$query = 'SELECT `date`, amount FROM transactions WHERE email = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND asset = 0 AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
 		$result = $this->getQueryResult($query);
 		$totalAssets = array(); // map from `date` (as string) to number
 		$sum = 0;
@@ -141,7 +141,7 @@ class WorthDB {
 	}
 
 	public function getNetWorths($startDate, $endDate) {
-		$query = 'SELECT `date`, amount FROM transactions WHERE email = "' . mysqli_real_escape_string($this->link, $this->email) . '" AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
+		$query = 'SELECT `date`, amount FROM transactions WHERE email = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND `date` >= "' . mysqli_real_escape_string($this->link, $startDate) . '" AND `date` <= "' . mysqli_real_escape_string($this->link, $endDate) . ' 23:59:59" ORDER BY `date` ASC;';
 		$result = $this->getQueryResult($query);
 		$netWorths = array(); // map from `date` (as string) to number
 		$sum = 0;
@@ -224,7 +224,7 @@ class WorthDB {
 	}
 
 	public function getTotalExpenditureOfCategory($category) {
-		$query = 'SELECT budget FROM budgets WHERE email="' . mysqli_real_escape_string($this->link, $this->email) . '" AND category = "' . mysqli_real_escape_string($this->link, $category) . '";';
+		$query = 'SELECT budget FROM budgets WHERE email=AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND category = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $category) . '", "thisispassword", @iv);';
 		$result = $this->getQueryResult($query);
 		$expenditureOfCategory = array();
 		foreach ($this->getCategories() as $cat) {
@@ -235,7 +235,7 @@ class WorthDB {
 		while ($row = $result->fetch_assoc()) {
 			$expenditureOfCategory['categoryName'] = $category;
 			$expenditureOfCategory['budget'] = number_format(floatval(str_replace(',', '', number_format($row['budget'], 2))), 2);
-			$query = 'SELECT amount FROM transactions WHERE email = "' . mysqli_real_escape_string($this->link, $this->email) . '" AND category = "' . mysqli_real_escape_string($this->link, $category) . '" AND date >= DATE_FORMAT(NOW(), "%Y-%m-01") AND date <= NOW();';
+			$query = 'SELECT amount FROM transactions WHERE email = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND category = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $category) . '", "thisispassword", @iv) AND date >= DATE_FORMAT(NOW(), "%Y-%m-01") AND date <= NOW();';
 			$resultTwo = $this->getQueryResult($query);
 			$expenditure = 0.;
 			while ($rowTwo = $resultTwo->fetch_assoc()) {
@@ -251,7 +251,7 @@ class WorthDB {
 		if (!in_array($category, $this->getCategories())) {
 			return false;
 		}
-		$query = 'DELETE FROM budgets WHERE email="' . mysqli_real_escape_string($this->link, $this->email) . '" AND category = "' . mysqli_real_escape_string($this->link, $category) . '";';
+		$query = 'DELETE FROM budgets WHERE email=AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $this->email) . '", "thisispassword", @iv) AND category = AES_ENCRYPT("' . mysqli_real_escape_string($this->link, $category) . '", "thisispassword", @iv);';
 		$result = $this->getQueryResult($query);
 		$query = 'INSERT INTO budgets (email, category, budget) VALUES ("' . mysqli_real_escape_string($this->link, $this->email) . '", "' . mysqli_real_escape_string($this->link, $category) . '", ' . mysqli_real_escape_string($this->link, $amount) . ');';
 		$result = $this->getQueryResult($query);
